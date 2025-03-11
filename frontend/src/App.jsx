0383e0f60610bb5c18af040d874eaba8c3fdf4bc
Ankit Mishra <am9491691@gmail.com>
2025-03-11 20:12:05 +0545

    Initialized frontend with Vite, Tailwind CSS, and React-Bootstrap

---
+++ frontend/src/App.jsx
@@ -0,0 +1,11 @@
+import { Button } from "react-bootstrap";
+
+function App() {
+  return (
+    <div className="d-flex justify-content-center align-items-center vh-100 bg-dark text-white">
+      <Button variant="dark">Click Me</Button>
+    </div>
+  );
+}
+
+export default App;
